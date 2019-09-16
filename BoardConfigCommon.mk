c1f17a45c0a74d36ac78e9cd6b071317c34b3e31
Rahul <rahul731999@gmail.com>
2019-09-16 03:29:13 +0000

    bacon: add temp sepolicies

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,9 +143,9 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy_tmp
 
 # TWRP
 ifeq ($(WITH_TWRP),true)
